d01796f6a0cb64ef5caeb72b2b4a1af27ea502b6
Malia <ioneaxelrod@gmail.com>
2018-01-20 21:52:33 -0800

    Added filereading capabilities and testing

--- src/main/scala/BadLibsCore/FileParser.scala
+++ src/main/scala/BadLibsCore/FileParser.scala
@@ -2,4 +2,10 @@ package BadLibsCore
 
 object FileParser {
 
+  def readBadLibsLines(filename:String): Seq[String] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    val lines = (for (line <- bufferedSource.getLines()) yield line).toList
+    bufferedSource.close
+    lines
+  }
 }
--- src/test/scala/BadLibsCore/FileParserTest.scala
+++ src/test/scala/BadLibsCore/FileParserTest.scala
@@ -1,5 +1,10 @@
 package BadLibsCore
 
-class FileParserTest {
+import org.scalatest.FunSuite
 
+class FileParserTest extends FunSuite {
+
+
+    val testSeq = Seq("joe", "maria", "kevin")
+    test("testReadBadLibsLines")(assert(testSeq.equals(FileParser.readBadLibsLines("/Users/Malia/IdeaProjects/Scala2/Scalaers/resource/names.txt"))))
 }
