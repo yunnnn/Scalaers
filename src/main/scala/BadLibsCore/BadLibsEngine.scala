240b2e1970865c435b638bd204ddd3ea5c113e59
yunfei.zong <yunfei.zong@workday.com>
2018-01-20 21:46:39 -0800

    badlibsengine

--- src/main/scala/BadLibsCore/BadLibsEngine.scala
+++ src/main/scala/BadLibsCore/BadLibsEngine.scala
@@ -1,9 +1,13 @@
 package BadLibsCore
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 object BadLibsEngine {
 =======
 object BadLibsMasher {
 >>>>>>> f5fd65c... Base Badlibs files
+=======
+object BadLibsEngine {
+>>>>>>> 3879dfd... Engine rename
 
 }
